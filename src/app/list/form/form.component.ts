9616594fbe2ff8f3a6e7e33ea0b63a783cf93f69
Matheus Marques <marquesluiz.matheus@gmail.com>
2019-08-22 14:02:03 -0300

    Editar e Adcionar usuário implementado

--- src/app/list/form/form.component.ts
+++ src/app/list/form/form.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, Input, Inject } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
 import { User } from '../user/User';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 import { LocalStorageService } from 'src/app/local-storage.service';
-import { isNull } from 'util';
+import { skip } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form',
@@ -26,22 +25,26 @@ export class FormComponent implements OnInit {
   website: string;
   
   constructor(
-    @Inject(LOCAL_STORAGE) private storage: StorageService,
+    @Inject(LOCAL_STORAGE) 
+    private storage: StorageService,
     private localStorageService: LocalStorageService,
     private route: ActivatedRoute,
     private router: Router
     ) { }
 
   ngOnInit() {
+    // REFATORAR ESSE CÓDIGO
+    // ----------------------------------------------------------
     //Buscar o id passado como parâmetro na url
     var usuario = this.route.snapshot.params;
-    console.log(usuario);
+    // console.log(usuario);
     this.id = +this.route.snapshot.paramMap.get("id");
     this.name = this.route.snapshot.paramMap.get("name");
     this.username = this.route.snapshot.paramMap.get("username");
     this.email = this.route.snapshot.paramMap.get("email");
     this.phone = this.route.snapshot.paramMap.get("phone");
     this.website = this.route.snapshot.paramMap.get("website");
+    // ----------------------------------------------------------
   }
 
   public onSubmit(){
@@ -71,42 +74,40 @@ export class FormComponent implements OnInit {
   }
 
   public hasUser(){
-    console.log("Has User");
-    if(this.user.id == null){
-      console.log("if");
-      this.newUser();
-    }else{
-      console.log("else");
-      this.updateUser();
-    }
+    let redirect: boolean = false;
+    this.users.forEach(user => {
+      if(!redirect){
+        if(this.getUserId(user) == this.user.id){
+          this.updateUser(user);
+          redirect = true;
+        } else{
+          this.newUser();
+          redirect = true;
+        }
+      }
+    });
   }
 
   public newUser(){
-    this.user.id = this.users[this.users.length - 1].id++;
-    this.users.splice(this.users.length+1, 1, this.user)
+    this.user.id = this.getLastUserId() + 1;
+    this.users.splice(this.users.length, 1, this.user);
   }
 
-  public updateUser(){
-    // console.log("Update")
-    this.users.forEach(user => {
-      // console.log("ForEach");
-      if(this.getUserId(user) == this.user.id){
-        // console.log("For Each if");
-        this.users[this.users.indexOf(user)] = this.user;
-        // console.log("User updated", user);
-      }
-    });
-    // console.log("Users updated ", this.users)
+  public updateUser(user: User){
+    this.users[this.users.indexOf(user)] = this.user;
   }
 
   public updateLocalStorage(){
-    console.log("Update local storage");
-    console.log(this.users)
     this.localStorageService.updateLocalStorage(this.users);
   }
 
   public getUserId(user: User){
     let index = this.users.indexOf(user);
     return this.users[index].id;
+  }
+
+  public getLastUserId(){
+    let id = this.users[this.users.length - 1].id;
+    return id;
   }
 }
